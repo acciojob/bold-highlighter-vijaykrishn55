de8185429a67e9bcdf05800824599264e3b33562
vijaykrishn55 <119862236+vijaykrishn55@users.noreply.github.com>
2023-07-31 22:02:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function highlight() {
     //Write your code here
-
+let boldwords= document.querySelectorAll('strong');
+	boldwords.forEach((word)=>{
+		word.style.color='green';
+	})
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+let boldwords=document.querySelectorAll('strong');
+	boldwords.forEach((word)=>{
+		word.style.color='black';
+	})
     
 }
